942fb1383e036585d21b6af93c2f4ecbbf04fd6a
jahangiralipour <alipourjahangir@gmail.com>
2025-03-11 10:39:16 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -8,7 +8,7 @@ Script Purpose:
     within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse_TSQL' database if it exists. 
+    Running this script will drop the entire 'DataWarehouse_TSQLL' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
@@ -17,10 +17,10 @@ USE master;
 GO
 
 -- Drop and recreate the 'DataWarehouse_TSQL' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse_TSQL')
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+    ALTER DATABASE DataWarehouse_TSQL SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse_TSQL;
 END;
 GO
 
